afe266b65e7094f896ed8c555651ac2a130d800b
Nathan Clark <Nathan.Clark@nasa.gov>
2016-01-21 19:51:01 -0500

    Generating syllabic/pronounceable passwords.

--- lib/syllabic.go
+++ lib/syllabic.go
@@ -1,5 +1,30 @@
 package lib
 
-import (
-	// "crypto/rand"
-)
+// Koremutake syllables, from http://shorl.com/koremutake
+var SYLLABLES = [...]string {
+	"ba", "be", "bi", "bo", "bu", "by", "da", "de", "di", "do", "du", "dy",
+	"fa", "fe", "fi", "fo", "fu", "fy", "ga", "ge", "gi", "go", "gu", "gy",
+	"ha", "he", "hi", "ho", "hu", "hy", "ja", "je", "ji", "jo", "ju", "jy",
+	"ka", "ke", "ki", "ko", "ku", "ky", "la", "le", "li", "lo", "lu", "ly",
+	"ma", "me", "mi", "mo", "mu", "my", "na", "ne", "ni", "no", "nu", "ny",
+	"pa", "pe", "pi", "po", "pu", "py", "ra", "re", "ri", "ro", "ru", "ry",
+	"sa", "se", "si", "so", "su", "sy", "ta", "te", "ti", "to", "tu", "ty",
+	"va", "ve", "vi", "vo", "vu", "vy", "bra", "bre", "bri", "bro", "bru",
+	"bry", "dra", "dre", "dri", "dro", "dru", "dry", "fra", "fre", "fri", "fro",
+	"fru", "fry", "gra", "gre", "gri", "gro", "gru", "gry", "pra", "pre", "pri",
+	"pro", "pru", "pry", "sta", "ste", "sti", "sto", "stu", "sty", "tra", "tre",
+}
+
+func randSyllable() string {
+	return SYLLABLES[randInt(len(SYLLABLES))]
+}
+
+func SyllabicPassword(length int) string {
+	var result string
+
+	for len(result) < length {
+		result += randSyllable()
+	}
+
+	return result
+}
--- main.go
+++ main.go
@@ -10,5 +10,6 @@ import (
 func main() {
 	fmt.Println(lib.RandomPasswordOpts(24, lib.DEFAULT_OPTIONS))
 	fmt.Println(lib.RandomPasswordOpts(24, lib.DEFAULT_OPTIONS ^ lib.INCLUDE_SPECIAL))
+	fmt.Println(lib.SyllabicPassword(24))
 	fmt.Println(lib.XKCDPassword(24))
 }
